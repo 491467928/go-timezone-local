2a935ea105bbd4184b2d3e3a85050e314481b4fd
wanxl <wanxl@idste.cn>
2024-06-24 10:44:08 +0800

    hide cmd console window

--- tzlocal/tz_windows_tzutil.go
+++ tzlocal/tz_windows_tzutil.go
@@ -6,11 +6,15 @@ package tzlocal
 import (
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
 // localTZfromTzutil executes command `tzutil /g` to get the name of the time zone Windows is configured to use.
 func localTZfromTzutil() (string, error) {
 	cmd := exec.Command("tzutil", "/g")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		HideWindow: true,
+	}
 	data, err := cmd.Output()
 	if err != nil {
 		return "", err
